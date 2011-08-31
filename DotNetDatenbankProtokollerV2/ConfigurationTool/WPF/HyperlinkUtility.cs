7bc48f851f2405cd80aabf014dd79718742014c6
jogibear9988 <none@none.com>
2011-08-31 05:36:22 +0000

    - 2 files I forgot to commit

---
+++ DotNetDatenbankProtokollerV2/ConfigurationTool/WPF/HyperlinkUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace DotNetSimaticDatabaseProtokollerConfigurationTool.WPF
+{
+    public static class HyperlinkUtility
+    {
+        public static readonly DependencyProperty LaunchDefaultBrowserProperty = DependencyProperty.RegisterAttached("LaunchDefaultBrowser", typeof(bool), typeof(HyperlinkUtility), new PropertyMetadata(false, HyperlinkUtility_LaunchDefaultBrowserChanged));
+
+        public static bool GetLaunchDefaultBrowser(DependencyObject d)
+        {
+            return (bool)d.GetValue(LaunchDefaultBrowserProperty);
+        }
+
+        public static void SetLaunchDefaultBrowser(DependencyObject d, bool value)
+        {
+            d.SetValue(LaunchDefaultBrowserProperty, value);
+        }
+
+        private static void HyperlinkUtility_LaunchDefaultBrowserChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            DependencyObject d = (DependencyObject)sender;
+            if ((bool)e.NewValue)
+                ((ContentElement)d).AddHandler(Hyperlink.RequestNavigateEvent, new RequestNavigateEventHandler(Hyperlink_RequestNavigateEvent));
+            else
+                ((ContentElement)d).RemoveHandler(Hyperlink.RequestNavigateEvent, new RequestNavigateEventHandler(Hyperlink_RequestNavigateEvent));
+        }
+
+        private static void Hyperlink_RequestNavigateEvent(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
+        }
+    }
+}
---
+++ DotNetDatenbankProtokollerV2/ProtokollerLibrary/Protocolling/Trigger/QuartzTriggerThread.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using DotNetSiemensPLCToolBoxLibrary.Communication;
+using DotNetSimaticDatabaseProtokollerLibrary.Databases.Interfaces;
+using DotNetSimaticDatabaseProtokollerLibrary.SettingsClasses.Connections;
+using DotNetSimaticDatabaseProtokollerLibrary.SettingsClasses.Datasets;
+using Quartz;
+using Quartz.Impl;
+
+namespace DotNetSimaticDatabaseProtokollerLibrary.Protocolling.Trigger
+{    
+    internal class QuartzTriggerThread : IDisposable
+    {
+        private IDBInterface dbInterface;
+        private DatasetConfig datasetConfig;
+        private Dictionary<ConnectionConfig, Object> activConnections;
+
+        public event ThreadExceptionEventHandler ThreadExceptionOccured;
+
+        private bool StartedAsService;
+
+        private IScheduler _sched;
+
+        internal class QuartzJob : IJob
+        {
+            public virtual void Execute(IJobExecutionContext context)
+            {
+                ((Action) context.JobDetail.JobDataMap["CallbackMethod"]).Invoke();
+            }
+        }
+
+        public QuartzTriggerThread(IDBInterface dbInterface, DatasetConfig datasetConfig, Dictionary<ConnectionConfig, Object> activConnections, bool StartedAsService)
+        {
+            this.StartedAsService = StartedAsService;
+            this.dbInterface = dbInterface;
+            this.datasetConfig = datasetConfig;
+            this.activConnections = activConnections;
+        }
+
+        public void StartTrigger()
+        {
+            // construct a scheduler factory
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            _sched = schedFact.GetScheduler();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<QuartzJob>()
+                .WithIdentity("job_" + datasetConfig.Name, "group1")
+                .Build();
+
+
+            DateTimeOffset runTime = DateBuilder.EvenMinuteDate(DateTimeOffset.UtcNow);
+
+            // Trigger the job to run on the next round minute
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger_" + datasetConfig.Name, "group1")
+                .WithCronSchedule(datasetConfig.CronTab)
+                //.StartAt(runTime)
+                .Build();
+
+            job.JobDataMap["CallbackMethod"] = new Action(TriggerCallback);
+
+            _sched.ScheduleJob(job, trigger);
+            _sched.Start();
+        }
+
+
+        private void TriggerCallback()
+        {
+            try
+            {
+                IEnumerable<object> values = ReadData.ReadDataFromPLCs(datasetConfig.DatasetConfigRows, activConnections, StartedAsService);
+                if (values != null)
+                    dbInterface.Write(values);
+            }
+            catch (ThreadAbortException ex)
+            {
+                ThreadExceptionOccured.Invoke(this, new ThreadExceptionEventArgs(ex));
+            }
+        }
+
+        public void Dispose()
+        {
+            _sched.Shutdown(true);
+        }
+        
+    }
+}
