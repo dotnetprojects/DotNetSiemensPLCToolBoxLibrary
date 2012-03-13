bb1882400b5784d096949cc8a2410aaf6a5c7c65
jogibear9988 <none@none.com>
2012-03-13 07:32:15 +0000

    Common Settings - Protokoller

---
+++ DotNetDatenbankProtokollerV2/ConfigurationTool/Windows/EditCommonSettings.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using DotNetSiemensPLCToolBoxLibrary.Communication;
+using DotNetSimaticDatabaseProtokollerLibrary;
+using DotNetSimaticDatabaseProtokollerLibrary.SettingsClasses.Connections;
+using MessageBox = System.Windows.MessageBox;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace DotNetSimaticDatabaseProtokollerConfigurationTool.Windows
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class EditCommonSettings : UserControl
+    {
+        public EditCommonSettings()
+        {
+            InitializeComponent();
+        }        
+    }
+}
