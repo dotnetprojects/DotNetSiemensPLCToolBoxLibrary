c231b3fca2f8b7c710484c9a6412984b4734f4db
jogibear9988 <jochen.kuehner@gmx.de>
2013-02-15 17:31:19 +0100

    Updates...

---
+++ SimpleCSharpService/Service.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+using DotNetSiemensPLCToolBoxLibrary.Communication;
+
+namespace SimpleCSharpService
+{
+    public partial class Service : ServiceBase
+    {
+
+        public Service()
+        {
+            ServiceName = "SimpleCSharpService";
+
+            InitializeComponent();
+        }
+
+        private Thread myThread;
+
+        private volatile bool threadShouldRun;
+
+        private PLCConnection myConn;
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+
+            var cfg = new PLCConnectionConfiguration("myConnection", LibNodaveConnectionConfigurationType.ObjectSavedConfiguration);
+            cfg.ConnectionType = (int) LibNodaveConnectionTypes.ISO_over_TCP;
+            cfg.CpuIP = "192.168.1.1";
+            cfg.CpuSlot = 2;
+
+            myConn = new PLCConnection(cfg);
+            myConn.Connect();
+            
+            threadShouldRun = true;
+
+            myThread = new Thread(new ThreadStart(this.ThreadProc));
+            myThread.Start();
+            
+                    
+        }
+
+        private void ThreadProc()
+        {
+            PLCTag tag = new PLCTag("MW0");
+            object oldValue = null;
+
+            while (threadShouldRun)
+            {
+                myConn.ReadValue(tag);
+
+                if (oldValue != tag.Value)
+                {
+                    //Hier Code was bei SPS Wertänderung passieren soll!
+
+
+                }
+
+                oldValue = tag.Value;
+                Thread.Sleep(100);
+            }            
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            threadShouldRun = false;
+        }
+    }
+}
---
+++ SimpleCSharpService/WindowsServiceInstaller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SimpleCSharpService
+{
+    [RunInstaller(true)]
+    public class WindowsServiceInstaller : Installer
+    {
+        /// <summary>
+        /// Public Constructor for WindowsServiceInstaller.
+        /// - Put all of your Initialization code here.
+        /// </summary>
+        public WindowsServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+            serviceInstaller.DisplayName = "SimpleCSharpService";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            //# This must be identical to the WindowsService.ServiceBase name
+            //# set in the constructor of WindowsService.cs
+            serviceInstaller.ServiceName = "SimpleCSharpService";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
