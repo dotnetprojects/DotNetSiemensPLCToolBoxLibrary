b7e242158ad595f74c9490cae44890f7f5847d3b
jogibear9988 <jochen.kuehner@gmx.de>
2013-02-15 17:40:23 +0100

    fixes...

--- SimpleCSharpService/Service.cs
+++ SimpleCSharpService/Service.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -12,6 +13,8 @@ using DotNetSiemensPLCToolBoxLibrary.Communication;
 
 namespace SimpleCSharpService
 {
+    //Installieren mit:
+    //C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil /i SimpleCSharpService.exe
     public partial class Service : ServiceBase
     {
 
@@ -34,7 +37,7 @@ namespace SimpleCSharpService
 
             var cfg = new PLCConnectionConfiguration("myConnection", LibNodaveConnectionConfigurationType.ObjectSavedConfiguration);
             cfg.ConnectionType = (int) LibNodaveConnectionTypes.ISO_over_TCP;
-            cfg.CpuIP = "192.168.1.1";
+            cfg.CpuIP = "192.168.1.185";
             cfg.CpuSlot = 2;
 
             myConn = new PLCConnection(cfg);
@@ -50,7 +53,7 @@ namespace SimpleCSharpService
 
         private void ThreadProc()
         {
-            PLCTag tag = new PLCTag("MW0");
+            PLCTag tag = new PLCTag("MD0");
             object oldValue = null;
 
             while (threadShouldRun)
@@ -61,6 +64,9 @@ namespace SimpleCSharpService
                 {
                     //Hier Code was bei SPS Wertänderung passieren soll!
 
+                    StreamWriter myFile = new StreamWriter("c:\\test.txt", true);
+                    myFile.Write(tag.Value + Environment.NewLine);
+                    myFile.Close();
 
                 }
 
