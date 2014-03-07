a0cc0173caf496d0fce7198c2084cf3f94806b50
jkuehner <jochen.kuehner@kardex.com>
2014-03-07 08:49:12 +0100

    libnodave fixes...

--- externalDlls/libnodave_patches/nodave.c
+++ externalDlls/libnodave_patches/nodave.c
@@ -1237,6 +1237,7 @@ int DECL2 daveListBlocksOfType(daveConnection * dc,uc type,daveBlockEntry * buf)
 		dc->_resultPointer=p2.udata;
 		len+=p2.udlen;
 		printf("more data\n");
+		pam[7]=p2.param[7];
 		res=daveBuildAndSendPDU(dc, &p2,pam, sizeof(pam), NULL, 1);
 		//if (res!=daveResOK) return res; 	// bugfix from Natalie Kather
 	}
