91ebace80f581ec560c12481764e6151d1857c74
jogibear9988 <jochen.kuehner@gmx.de>
2014-08-26 13:48:10 +0200

    Fixes in Unit Tests

--- ToolBoxLibUnitTests/TestLibNoDaveReading.cs
+++ ToolBoxLibUnitTests/TestLibNoDaveReading.cs
@@ -35,7 +35,7 @@ READ  Area:132, DBnum:60, Start:0, Bytes:300";
             var pdus2 = wrapper2.PDUs;
             string req2 = string.Join(Environment.NewLine, pdus2);
             string t2 = "READ  Area:132, DBnum:10, Start:10, Bytes:300\r\nREAD  Area:132, DBnum:60, Start:0, Bytes:300";
-            Assert.AreEqual(req, t);
+            Assert.AreEqual(req2, t2);
 
             var wrapper3 = new ConnectionWrapper(480);
             var conn3 = new PLCConnection(new PLCConnectionConfiguration(), wrapper3);
@@ -46,7 +46,22 @@ READ  Area:132, DBnum:60, Start:0, Bytes:300";
             var pdus3 = wrapper3.PDUs;
             string req3 = string.Join(Environment.NewLine, pdus3);
             string t3 = "READ  Area:132, DBnum:10, Start:10, Bytes:300\r\nREAD  Area:132, DBnum:60, Start:0, Bytes:140\r\nREAD  Area:132, DBnum:60, Start:140, Bytes:160";
-            Assert.AreEqual(req, t);
+            Assert.AreEqual(req3, t3);
+
+            var wrapper4 = new ConnectionWrapper(480);
+            var conn4 = new PLCConnection(new PLCConnectionConfiguration(), wrapper4);
+            var listTag4 = new List<PLCTag>();
+            listTag4.Add(new PLCTag("P#DB1.DBX0 BYTE 4") { DontSplitValue = false });
+            listTag4.Add(new PLCTag("P#DB1.DBX8 BYTE 4") { DontSplitValue = false });
+            listTag4.Add(new PLCTag("P#DB1.DBX10 BYTE 4") { DontSplitValue = false });
+            listTag4.Add(new PLCTag("P#DB1.DBX30 BYTE 4") { DontSplitValue = false });
+            listTag4.Add(new PLCTag("P#DB1.DBX50 BYTE 4") { DontSplitValue = false });
+            listTag4.Add(new PLCTag("P#DB1.DBX80 BYTE 4") { DontSplitValue = false });
+            conn4.ReadValues(listTag4, true);
+            var pdus4 = wrapper4.PDUs;
+            string req4 = string.Join(Environment.NewLine, pdus4);
+            string t4 = "READ  Area:132, DBnum:1, Start:0, Bytes:14\r\nREAD  Area:132, DBnum:1, Start:30, Bytes:4\r\nREAD  Area:132, DBnum:1, Start:50, Bytes:4\r\nREAD  Area:132, DBnum:1, Start:80, Bytes:4";
+            Assert.AreEqual(req4, t4);
         }
     }
 }
