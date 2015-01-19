d4ef17f536000819183bfdabccfc502f8f22142a
jkuehner <jochen.kuehner@kardex.com>
2015-01-19 15:49:47 +0100

    fix in Protokoller

---
+++ DotNetDatenbankProtokollerV2/ProtokollerLibrary/Common/IpAddressConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DotNetSimaticDatabaseProtokollerLibrary.Common
+{
+    public class IpAddressConverter : JsonConverter
+    {
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString());
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return IPAddress.Parse(reader.Value.ToString());
+            //var chars = ((string) existingValue).Split(',');
+            //return new Size(double.Parse(chars[0], CultureInfo.InvariantCulture), double.Parse(chars[1], CultureInfo.InvariantCulture));
+        }
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(IPAddress);
+        }
+
+    }
+}
