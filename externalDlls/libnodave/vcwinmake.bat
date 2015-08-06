9e9f202097204314293bae2792dd91d54cc1c64f
jkuehner <jochen.kuehner@kardex.com>
2015-08-06 09:42:06 +0200

    Run Batch per call

--- externalDlls/libnodave/vcwinmake.bat
+++ externalDlls/libnodave/vcwinmake.bat
@@ -1,3 +1,3 @@
 set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\nmake"
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 %PATH_TO_MAKE% /f MAKEFILE.VC %1
--- externalDlls/libnodave/vcwinmake_64.bat
+++ externalDlls/libnodave/vcwinmake_64.bat
@@ -1,3 +1,3 @@
 set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\nmake"
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
 %PATH_TO_MAKE% /f MAKEFILE.64 %1
