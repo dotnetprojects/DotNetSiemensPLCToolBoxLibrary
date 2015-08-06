dfe5b76780a6c333a9e066edac7d2aee31a2c0aa
jkuehner <jochen.kuehner@kardex.com>
2015-08-06 09:52:52 +0200

    Fix Make Batch

--- externalDlls/libnodave/vcwinmake.bat
+++ externalDlls/libnodave/vcwinmake.bat
@@ -1,4 +1,7 @@
 set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 cd externalDlls\libnodave\
+del *.obj
 %PATH_TO_MAKE% /f MAKEFILE.VC %1
+cd ..
+cd ..
