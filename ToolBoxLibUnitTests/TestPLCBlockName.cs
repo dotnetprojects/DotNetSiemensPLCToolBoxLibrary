33f084e4b7fccaecbce862eec4dccf8551edbe48
jogibear9988 <jochen.kuehner@gmx.de>
2017-01-02 23:17:56 +0100

    Use nunit so VS for Mac could be used

--- ToolBoxLibUnitTests/TestPLCBlockName.cs
+++ ToolBoxLibUnitTests/TestPLCBlockName.cs
@@ -1,52 +1,51 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DotNetSiemensPLCToolBoxLibrary.DataTypes;
+﻿using DotNetSiemensPLCToolBoxLibrary.DataTypes;
+using NUnit.Framework;
 
 namespace ToolBoxLibUnitTests
 {
-    [TestClass]
-    public class TestPLCBlockName
-    {
-        [TestMethod]
-        public void TestParse()
-        {
-            PLCBlockName test = new PLCBlockName("DB5");
-            Assert.AreEqual(5, test.BlockNumber);
-            Assert.AreEqual(PLCBlockType.DB, test.BlockType);
+	[TestFixture]
+	public class TestPLCBlockName
+	{
+		[Test]
+		public void TestParse()
+		{
+			PLCBlockName test = new PLCBlockName("DB5");
+			Assert.AreEqual(5, test.BlockNumber);
+			Assert.AreEqual(PLCBlockType.DB, test.BlockType);
 
-            test = new PLCBlockName("db 5");
-            Assert.AreEqual(5, test.BlockNumber);
-            Assert.AreEqual(PLCBlockType.DB, test.BlockType);
-            
-            test = new PLCBlockName("FC109");
-            Assert.AreEqual(109, test.BlockNumber);
-            Assert.AreEqual(PLCBlockType.FC, test.BlockType);
+			test = new PLCBlockName("db 5");
+			Assert.AreEqual(5, test.BlockNumber);
+			Assert.AreEqual(PLCBlockType.DB, test.BlockType);
 
-            test = new PLCBlockName("FB9999");
-            Assert.AreEqual(9999, test.BlockNumber);
-            Assert.AreEqual(PLCBlockType.FB, test.BlockType);
+			test = new PLCBlockName("FC109");
+			Assert.AreEqual(109, test.BlockNumber);
+			Assert.AreEqual(PLCBlockType.FC, test.BlockType);
 
-            test = new PLCBlockName("SDB189");
-            Assert.AreEqual(189, test.BlockNumber);
-            Assert.AreEqual(PLCBlockType.SDB, test.BlockType);
+			test = new PLCBlockName("FB9999");
+			Assert.AreEqual(9999, test.BlockNumber);
+			Assert.AreEqual(PLCBlockType.FB, test.BlockType);
 
-            test = new PLCBlockName("OB1");
-            Assert.AreEqual(1, test.BlockNumber);
-            Assert.AreEqual(PLCBlockType.OB, test.BlockType);
+			test = new PLCBlockName("SDB189");
+			Assert.AreEqual(189, test.BlockNumber);
+			Assert.AreEqual(PLCBlockType.SDB, test.BlockType);
 
-            try
-            {
-                test = new PLCBlockName("FX77");
-                Assert.Fail("Parsing invalid Block types must fail");
-            }
-            catch { } //Must throw error
+			test = new PLCBlockName("OB1");
+			Assert.AreEqual(1, test.BlockNumber);
+			Assert.AreEqual(PLCBlockType.OB, test.BlockType);
 
-            try
-            {
-                test = new PLCBlockName("FC7a");
-                Assert.Fail("Parsing invalid Block numbers must fail");
-            }
-            catch { } //Must throw error
-        }
-    }
+			try
+			{
+				test = new PLCBlockName("FX77");
+				Assert.Fail("Parsing invalid Block types must fail");
+			}
+			catch { } //Must throw error
+
+			try
+			{
+				test = new PLCBlockName("FC7a");
+				Assert.Fail("Parsing invalid Block numbers must fail");
+			}
+			catch { } //Must throw error
+		}
+	}
 }
