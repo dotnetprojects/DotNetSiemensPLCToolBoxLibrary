a9b731dcf73028c5eeb8c58205fb0c55af60fbef
jkuehner <jochen.kuehner@kardex.com>
2018-05-22 21:00:11 +0200

    libnodave to vs14

--- externalDlls/libnodave/vcwinmake.bat
+++ externalDlls/libnodave/vcwinmake.bat
@@ -1,5 +1,5 @@
-set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake"
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\nmake"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 cd externalDlls\libnodave\
 del *.obj
 %PATH_TO_MAKE% /f MAKEFILE.VC %1
--- externalDlls/libnodave/vcwinmake_64.bat
+++ externalDlls/libnodave/vcwinmake_64.bat
@@ -1,5 +1,5 @@
-set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake"
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+set PATH_TO_MAKE="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\nmake"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cd externalDlls\libnodave\
 del *.obj
 %PATH_TO_MAKE% /f MAKEFILE.64 %1
