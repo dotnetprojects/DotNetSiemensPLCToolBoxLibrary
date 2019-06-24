5531106bba257fff7392273eb9c49221cbe8db04
Sven Knoerzer <sven.knoerzer@kardex.com>
2019-06-24 09:42:18 +0200

    Remove temp fix for not exported setpoint attributes in TiaGitHandler.

--- TiaGitHandler/Program.cs
+++ TiaGitHandler/Program.cs
@@ -674,59 +674,6 @@ namespace TiaGitHandler
                                         }
                                     }
 
-                                    //// add setpoint node if not existing (necessary because TIA sets setpoint true if no node exists during import)
-                                    //try
-                                    //{
-                                    //    var nodes = xmlDoc2.SelectNodes("//smns2:Member/*[local-name()= \"AttributeList\"]", ns2);
-                                    //    foreach (var node in nodes.Cast<XmlNode>())
-                                    //    {
-                                    //        // get section node
-                                    //        XmlNode tmpNode = node;
-                                    //        var isNoSectionToAddSetpointNode = false;
-                                    //        while (tmpNode.ParentNode != null)
-                                    //        {
-                                    //            if (tmpNode.ParentNode.Name == "Section")
-                                    //            {
-                                    //                if (tmpNode.ParentNode.Attributes["Name"].Value == "Input" ||
-                                    //                    tmpNode.ParentNode.Attributes["Name"].Value == "Output" ||
-                                    //                    tmpNode.ParentNode.Attributes["Name"].Value == "InOut" ||
-                                    //                    tmpNode.ParentNode.Attributes["Name"].Value == "Constant" ||
-                                    //                    tmpNode.ParentNode.Attributes["Name"].Value == "Temp")
-                                    //                {
-                                    //                    isNoSectionToAddSetpointNode = true;
-                                    //                    break;
-                                    //                }
-                                    //            }
-
-                                    //            tmpNode = tmpNode.ParentNode;
-                                    //        }
-
-                                    //        if (!isNoSectionToAddSetpointNode && projectBlockInfo.BlockType != PLCBlockType.FC)
-                                    //        {
-                                    //            var setPointAttributeExists = false;
-                                    //            foreach (var node2 in node.ChildNodes.Cast<XmlNode>())
-                                    //            {
-                                    //                if (node2.Attributes["Name"].Value == "SetPoint")
-                                    //                {
-                                    //                    setPointAttributeExists = true;
-                                    //                }
-                                    //            }
-
-                                    //            if (!setPointAttributeExists)
-                                    //            {
-                                    //                XmlElement booleanAttribute = xmlDoc2.CreateElement("BooleanAttribute", node.NamespaceURI);
-                                    //                booleanAttribute.SetAttribute("Name", "SetPoint");
-                                    //                booleanAttribute.SetAttribute("SystemDefined", "true");
-                                    //                booleanAttribute.InnerXml = "false";
-                                    //                node.AppendChild(booleanAttribute);
-                                    //            }
-                                    //        }                                          
-                                    //    }
-                                    //}
-                                    //catch
-                                    //{
-                                    //}
-
                                     if (resetSetpoints)
                                     {
                                         try
