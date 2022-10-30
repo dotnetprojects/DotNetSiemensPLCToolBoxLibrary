522ded9827e5d5664e298a7bf23e28c8721bee1d
jkuehner <jochen.kuehner@kardex.com>
2022-10-30 23:19:59 +0100

    commit missing file

---
+++ LibNoDaveConnectionLibrary/General/ByteArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetSiemensPLCToolBoxLibrary.General
+{
+    public static class ByteArrayExtensions
+    {
+        public static int IndexOfBytes(this byte[] array, byte[] pattern, int startIndex = 0)
+        {
+            int i = startIndex;
+            int endIndex = array.Length;
+            int fidx = 0;
+
+            while (i++ < endIndex)
+            {
+                fidx = (array[i] == pattern[fidx]) ? ++fidx : 0;
+                if (fidx == pattern.Length)
+                {
+                    return i - fidx + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
