9ab6d3ab106ff003f4ba99d22cf55ef2b042b464
Sven Knoerzer <sven.knoerzer@kardex.com>
2025-04-01 11:32:28 +0200

    Fixes #232: Subfolders can't be created in variables folder

--- DotNetSiemensPLCToolBoxLibrary.TIAV19/Step7ProjectV19Tia.cs
+++ DotNetSiemensPLCToolBoxLibrary.TIAV19/Step7ProjectV19Tia.cs
@@ -458,7 +458,17 @@ namespace DotNetSiemensPLCToolBoxLibrary.Projectfiles.V19
                     return retVal;
                 }
             }
-            
+
+            public override ProjectFolder CreateFolder(string name)
+            {
+                var gp = group.Groups.Create(name);
+                var newFld = new TIAOpennessVariablesFolder((Step7ProjectV19)Project, ControllerFolder, gp);
+                newFld.Name = gp.Name;
+                newFld.Parent = this;
+                this.SubItems.Add(newFld);
+                return newFld;
+            }
+
             public override void ImportFile(FileInfo file, bool overwrite, bool importFromSource)
             {
                 this.group.TagTables.Import(file, overwrite ? ImportOptions.Override : ImportOptions.None);
